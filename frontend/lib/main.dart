b0258bd9e13ed64baae4693797e9ebaff1dbb02e
joel.perez.bermudez <joel.perez.bermudez@estudiantat.upc.edu>
2026-05-20 11:05:02 +0200

    feat: add home screen for frontend application

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/providers/auth_provider.dart';
@@ -9,7 +10,9 @@ import 'features/inventari/providers/inventory_provider.dart';
 import 'features/products/providers/products_provider.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  await initializeDateFormatting('ca', null);
   runApp(const MyApp());
 }
 
