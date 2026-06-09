3a6abdff0fc8fc48fe9ff68f687c3555ae1613c9
joel.perez.bermudez <joel.perez.bermudez@estudiantat.upc.edu>
2026-06-09 18:13:56 +0200

    chore: store database host in .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'ep-jolly-cloud-alm2kmi0.c-3.eu-central-1.aws.neon.tech',#'ep-jolly-cloud-alm2kmi0.c-3.eu-central-1.aws.neon.tech',#'localhost'
+        'HOST': os.getenv('HOST'),
         'PORT': '5432',
         'OPTIONS': {
             'sslmode': 'require',
